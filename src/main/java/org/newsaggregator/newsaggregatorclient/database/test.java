ad4108ae8c0dc9673d2c148b797e5bd13ed5d15d
Long Ly <longhienly112@gmail.com>
2024-05-21 15:15:37 +0700

    Adding SQLite support

--- src/main/java/org/newsaggregator/newsaggregatorclient/database/test.java
+++ src/main/java/org/newsaggregator/newsaggregatorclient/database/test.java
@@ -19,7 +19,24 @@ public class test {
         item1.setPublisher("publisher1");
         item1.setPublisherLogoURL("publisherLogoUrl1");
         item1.setCategory(List.of("category1", "category2"));
-        db.insert(item1);
-        db.select();
+//        db.insert(item1);
+        NewsItemData item2 = new NewsItemData();
+        item2.setGuid("guid2");
+        item2.setTitle("title2");
+        item2.setAuthor("author2");
+        item2.setDescription("description2");
+        item2.setArticleDetailedContent("content2");
+        item2.setUrl("url2");
+        item2.setUrlToImage("urlToImage2");
+        item2.setPublishedAt("publishedAt2");
+        item2.setPublisher("publisher2");
+        item2.setPublisherLogoURL("publisherLogoUrl2");
+        item2.setCategory(List.of("category3", "category4"));
+//        db.insert(item2);
+        db.delete("guid2");
+        List<NewsItemData> list = db.select();
+        for (NewsItemData item : list) {
+            item.printNewsData();
+        }
     }
 }
