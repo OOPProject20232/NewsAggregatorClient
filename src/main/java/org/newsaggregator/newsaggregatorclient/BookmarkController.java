cb21524976ae30108dc1ee1a59a2e58db48005c1
Hung Tran <hungtran30112004@gmail.com>
2024-05-22 23:28:37 +0700

    add infinite scrolling for bookmarks

--- src/main/java/org/newsaggregator/newsaggregatorclient/BookmarkController.java
+++ src/main/java/org/newsaggregator/newsaggregatorclient/BookmarkController.java
@@ -1,13 +1,18 @@
 package org.newsaggregator.newsaggregatorclient;
 
 import javafx.application.HostServices;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import org.newsaggregator.newsaggregatorclient.database.SQLiteJDBC;
 import org.newsaggregator.newsaggregatorclient.datamodel.NewsItemData;
 import org.newsaggregator.newsaggregatorclient.ui_components.datacard.NewsItemCard;
 import org.newsaggregator.newsaggregatorclient.ui_components.datagroupframes.CategoryTitledPane;
+import org.newsaggregator.newsaggregatorclient.ui_components.datagroupframes.InfiniteNews;
 import org.newsaggregator.newsaggregatorclient.ui_components.datagroupframes.NewsCategoryGroupTitledPane;
 import org.newsaggregator.newsaggregatorclient.ui_components.uiloader.ArticleItemsLoader;
 
@@ -17,8 +22,16 @@ public class BookmarkController {
     @FXML
     VBox bookmarkedGridPane;
 
+    @FXML
+    Button reload;
+
+    @FXML
+    ScrollPane bookmarkScrollPane;
+
     HostServices hostServices;
     NewsAggregatorClientController mainController;
+    int currentPage = 0;
+    int limit = 10;
 
     public BookmarkController(HostServices hostServices, NewsAggregatorClientController mainController) {
         this.hostServices = hostServices;
@@ -27,17 +40,43 @@ public class BookmarkController {
 
     @FXML
     public void initialize() {
-        NewsCategoryGroupTitledPane newsCategoryGroupTitledPane = new NewsCategoryGroupTitledPane("");
+        bookmarkedGridPane.getChildren().clear();
+        InfiniteNews newsCategoryGroupTitledPane = new InfiniteNews("");
         bookmarkedGridPane.getChildren().add(newsCategoryGroupTitledPane);
         newsCategoryGroupTitledPane.setPrefWidth(1000);
-        List<NewsItemData> data = getBookmarkedNews();
-        ArticleItemsLoader<CategoryTitledPane> articleItemsLoader = new ArticleItemsLoader<>(10, 0, hostServices, newsCategoryGroupTitledPane, mainController);
-        articleItemsLoader.loadItems(data);
+        Platform.runLater(() -> {
+            List<NewsItemData> data = getBookmarkedNews();
+            ArticleItemsLoader<CategoryTitledPane> articleItemsLoader = new ArticleItemsLoader<>(10, currentPage * limit, hostServices, newsCategoryGroupTitledPane, mainController);
+            articleItemsLoader.loadItems(data);
+        });
+        reload.setOnAction(e -> {
+            currentPage = 0;
+            handle(e);
+        });
+        bookmarkScrollPane.setOnScroll(e -> {
+            if (e.getDeltaY() > 0) {
+                currentPage--;
+            } else {
+                currentPage++;
+            }
+            if (currentPage < 0) {
+                currentPage = 0;
+            }
+            Platform.runLater(() -> {
+                List<NewsItemData> data = getBookmarkedNews();
+                ArticleItemsLoader<CategoryTitledPane> articleItemsLoader = new ArticleItemsLoader<>(10, currentPage * limit, hostServices, newsCategoryGroupTitledPane, mainController);
+                articleItemsLoader.loadItems(data);
+            });
+        });
     }
 
     private List<NewsItemData> getBookmarkedNews(){
         SQLiteJDBC db = new SQLiteJDBC();
         List<NewsItemData> data = db.select();
         return data;
+    }
+
+    private void handle(ActionEvent e) {
+        initialize();
     }
 }
